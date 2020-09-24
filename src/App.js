083eb368e85895ff6316fa821223f2deb5ea48a0
sunny812546 <sun812546@gmail.com>
2020-09-24 11:10:11 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
-function App() {
+const App = () => {
+  const endPointApi = "https://randomuser.me/api/";
+  const [data, setData] = useState({});
+  const fetchData = async () => {
+    try {
+      const { status, data } = await Axios.get(endPointApi);
+      console.log(data.results[0]);
+      if (status === 200) {
+        console.log("response is okay");
+        setData(data.results[0]);
+      } else {
+        throw new Error("Something gonna be wrong!");
+      }
+    } catch (err) {
+      console.log("error", err);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <img src={data.picture?.large} height="100px" width="100px"></img>
+      <h3>Age: {data.dob?.age}</h3>
+      <h3>
+        Name: {data.name?.title + " "} {data.name?.first}
+        {" " + data.name?.last}
+      </h3>
     </div>
   );
-}
+};
 
 export default App;
